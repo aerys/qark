ebeacb9988b335bee0ed429999452b43f9fd7bf2
promethe <promethe@promethe-VirtualBox.(none)>
2011-05-11 15:19:27 +0200

    internal functions are now private in the PHP5 port

--- php5/src/Qark.php
+++ php5/src/Qark.php
@@ -53,7 +53,7 @@ class Qark
                || 0 !== count(array_diff_key($array, array_keys(array_keys($array)))));
   }
 
-  public static function getType($value)
+  private static function getType($value)
   {
     if (self::isUnsignedInt($value))
       return self::TYPE_UINT;
@@ -134,7 +134,7 @@ class Qark
     return $value;
   }
 
-  public static function encodeRecursive($source, $target)
+  private static function encodeRecursive($source, $target)
   {
     $type = self::getType($source);
     $target = self::writeByte($target, $type);
@@ -143,7 +143,7 @@ class Qark
     return self::$encoder($source, $target);
   }
 
-  public static function decodeRecursive($source)
+  private static function decodeRecursive($source)
   {
     list($type, $source) = self::readByte($source);
     $decoder = self::$DECODERS[$type];
@@ -151,47 +151,47 @@ class Qark
     return self::$decoder($source);
   }
 
-  public static function encodeInteger($source, $target)
+  private static function encodeInteger($source, $target)
   {
     return self::writeInteger($target, $source);
   }
 
-  public static function decodeInteger($source)
+  private static function decodeInteger($source)
   {
     return self::readInteger($source);
   }
 
-  public static function encodeUnsignedInteger($source, $target)
+  private static function encodeUnsignedInteger($source, $target)
   {
     return self::writeUnsignedInteger($target, $source);
   }
 
-  public static function decodeUnsignedInteger($source)
+  private static function decodeUnsignedInteger($source)
   {
     return self::readUnsignedInteger($source);
   }
 
-  public static function encodeFloat($source, $target)
+  private static function encodeFloat($source, $target)
   {
     return self::writeFloat($target, $source);
   }
 
-  public static function decodeFloat($source)
+  private static function decodeFloat($source)
   {
     return self::readFloat($source);
   }
 
-  public static function encodeString($source, $target)
+  private static function encodeString($source, $target)
   {
     return self::writeUTF($target, $source);
   }
 
-  public static function decodeString($source)
+  private static function decodeString($source)
   {
     return self::readUTF($source);
   }
 
-  public static function encodeObject($source, $target)
+  private static function encodeObject($source, $target)
   {
     $target = self::writeShort($target, count($source));
 
@@ -204,7 +204,7 @@ class Qark
     return $target;
   }
 
-  public static function decodeObject($source)
+  private static function decodeObject($source)
   {
     $object = array();
 
@@ -223,7 +223,7 @@ class Qark
     return array($object, $source);
   }
 
-  public static function encodeArray($source, $target)
+  private static function encodeArray($source, $target)
   {
     $target = self::writeShort($target, count($source));
 
@@ -233,7 +233,7 @@ class Qark
     return $target;
   }
 
-  public static function decodeArray($source)
+  private static function decodeArray($source)
   {
     $array = array();
 
@@ -251,17 +251,17 @@ class Qark
     return array($array, $source);
   }
 
-  public static function encodeCustomObject($source, $target)
+  private static function encodeCustomObject($source, $target)
   {
     return self::encodeObject($source, $target);
   }
 
-  public static function decodeCustomObject($source)
+  private static function decodeCustomObject($source)
   {
     return self::decodeObject($source);
   }
 
-  public static function encodeBitmapData($source, $target)
+  private static function encodeBitmapData($source, $target)
   {
     $width = imagesx($source);
     $height = imagesy($source);
@@ -276,7 +276,7 @@ class Qark
     return $target;
   }
 
-  public static function decodeBitmapData($source)
+  private static function decodeBitmapData($source)
   {
     if (!function_exists('imagecreatetruecolor'))
       throw new Exception('Unable to decode bitmap data: the GD extension is '
@@ -300,54 +300,54 @@ class Qark
     return array($img, $source);
   }
 
-  public static function encodeBytes($source, $target)
+  private static function encodeBytes($source, $target)
   {
     $target = self::writeInteger(strlen($source));
 
     return self::writeBytes($source, $target);
   }
 
-  public static function decodeBytes($source)
+  private static function decodeBytes($source)
   {
     list($length, $source) = self::readInteger($source);
 
     return self::readBytes($source, $length);
   }
 
-  public static function encodeBoolean($source, $target)
+  private static function encodeBoolean($source, $target)
   {
     return self::writeByte($target, $source ? 1 : 0);
   }
 
-  public static function decodeBoolean($source)
+  private static function decodeBoolean($source)
   {
     list($value, $source) = self::readByte($source);
 
     return array($value != 0, $source);
   }
 
-  public static function readByte($source)
+  private static function readByte($source)
   {
     $byte = unpack('C', $source);
 
     return array((int)$byte[1], substr($source, 1));
   }
 
-  public static function writeByte($source, $byte)
+  private static function writeByte($source, $byte)
   {
     $source .= pack('C', (int)$byte);
 
     return $source;
   }
 
-  public static function readBytes($source, $length)
+  private static function readBytes($source, $length)
   {
     $bytes = unpack('C' . $length, $source);
 
     return array($bytes, substr($source, $length));
   }
 
-  public static function writeBytes($source, $bytes)
+  private static function writeBytes($source, $bytes)
   {
     for ($i = 0; $i < $length; $i++)
       $source = self::writeByte($source, $bytes[$i]);
@@ -355,63 +355,63 @@ class Qark
     return $source;
   }
 
-  public static function readShort($source)
+  private static function readShort($source)
   {
     $short = unpack('n', $source);
 
     return array((int)$short[1], substr($source, 2));
   }
 
-  public static function writeShort($source, $short)
+  private static function writeShort($source, $short)
   {
     $source .= pack('n', (int)$short);
 
     return $source;
   }
 
-  public static function readInteger($source)
+  private static function readInteger($source)
   {
     $integer = unpack('i', $source);
 
     return array((int)$interger[1], substr($source, 4));
   }
 
-  public static function writeInteger($source, $integer)
+  private static function writeInteger($source, $integer)
   {
     $source .= pack('i', (int)$integer);
 
     return $source;
   }
 
-  public static function readUnsignedInteger($source)
+  private static function readUnsignedInteger($source)
   {
     $integer = unpack('I', $source);
 
     return array((int)$integer[1], substr($source, 4));
   }
 
-  public static function writeUnsignedInteger($source, $integer)
+  private static function writeUnsignedInteger($source, $integer)
   {
     $source .= pack('I', (int)$integer);
 
     return $source;
   }
 
-  public static function readFloat($source)
+  private static function readFloat($source)
   {
     $float = unpack('f', $source);
 
     return array((float)$float, substr($source, 4));
   }
 
-  public static function writeFloat($source, $float)
+  private static function writeFloat($source, $float)
   {
     $source .= pack('f', (float)$float);
 
     return $source;
   }
 
-  public static function readUTF($source)
+  private static function readUTF($source)
   {
     list($length, $source) = self::readShort($source);
     list($bytes, $source) = self::readBytes($source, $length);
@@ -423,7 +423,7 @@ class Qark
     return array($string, $source);
   }
 
-  public static function writeUTF($source, $string)
+  private static function writeUTF($source, $string)
   {
     $length = strlen($string);
 
